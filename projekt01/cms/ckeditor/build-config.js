8189200a6bab7b5f9e72791785947218d446e25e
Holger Stitz <git@holgerstitz.de>
2014-02-18 21:54:34 +0100

    Updated CKEditor to v4.3.2

--- projekt01/cms/ckeditor/build-config.js
+++ projekt01/cms/ckeditor/build-config.js
@@ -1,14 +1,25 @@
-﻿
-/**
+﻿/**
  * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 /**
  * This file was added automatically by CKEditor builder.
- * You may re-use it at any time at http://ckeditor.com/builder to build CKEditor again.
- * 
- * NOTE: 
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/de8c5420872c7b4628c0186e96a23458
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/de8c5420872c7b4628c0186e96a23458
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
  *    This file is not used by CKEditor, you may remove it.
  *    Changing this file will not change your CKEditor configuration.
  */
@@ -24,53 +35,53 @@ var CKBUILDER_CONFIG = {
 		'.mailmap'
 	],
 	plugins : {
-		'about' : 1,
 		'a11yhelp' : 1,
+		'about' : 1,
 		'basicstyles' : 1,
 		'blockquote' : 1,
+		'button' : 1,
 		'clipboard' : 1,
 		'contextmenu' : 1,
-		'resize' : 1,
-		'toolbar' : 1,
+		'dialog' : 1,
+		'dialogui' : 1,
 		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
+		'fakeobjects' : 1,
 		'filebrowser' : 1,
 		'floatingspace' : 1,
+		'floatpanel' : 1,
 		'format' : 1,
-		'htmlwriter' : 1,
 		'horizontalrule' : 1,
-		'wysiwygarea' : 1,
+		'htmlwriter' : 1,
 		'image' : 1,
 		'indent' : 1,
+		'indentlist' : 1,
 		'link' : 1,
 		'list' : 1,
+		'listblock' : 1,
 		'magicline' : 1,
 		'maximize' : 1,
-		'pastetext' : 1,
+		'menu' : 1,
+		'menubutton' : 1,
+		'panel' : 1,
 		'pastefromword' : 1,
+		'pastetext' : 1,
+		'popup' : 1,
 		'removeformat' : 1,
+		'resize' : 1,
+		'richcombo' : 1,
+		'scayt' : 1,
 		'sourcearea' : 1,
 		'specialchar' : 1,
-		'scayt' : 1,
 		'stylescombo' : 1,
 		'tab' : 1,
 		'table' : 1,
 		'tabletools' : 1,
+		'toolbar' : 1,
 		'undo' : 1,
 		'wsc' : 1,
-		'dialog' : 1,
-		'dialogui' : 1,
-		'menu' : 1,
-		'floatpanel' : 1,
-		'panel' : 1,
-		'button' : 1,
-		'popup' : 1,
-		'richcombo' : 1,
-		'listblock' : 1,
-		'fakeobjects' : 1,
-		'menubutton' : 1,
-		'indentlist' : 1
+		'wysiwygarea' : 1
 	},
 	languages : {
 		'de' : 1,
