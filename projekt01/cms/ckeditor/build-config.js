124f9e63e9ee972f389ae5b02f8d85cde7dda864
Holger Stitz <git@holgerstitz.de>
2014-12-26 01:11:47 +0100

    Updated CKEditor to v4.4.6

--- projekt01/cms/ckeditor/build-config.js
+++ projekt01/cms/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/de8c5420872c7b4628c0186e96a23458
+ * (2) http://ckeditor.com/builder/4fb02d6c649621666a5939aa263b7e39
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/de8c5420872c7b4628c0186e96a23458
+ * (3) http://ckeditor.com/builder/download/4fb02d6c649621666a5939aa263b7e39
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -28,11 +28,24 @@ var CKBUILDER_CONFIG = {
 	skin: 'moonocolor',
 	preset: 'standard',
 	ignore: [
+		'.bender',
+		'bender.js',
+		'bender-err.log',
+		'bender-out.log',
 		'dev',
-		'.gitignore',
+		'.DS_Store',
 		'.gitattributes',
+		'.gitignore',
+		'gruntfile.js',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'node_modules',
+		'package.json',
 		'README.md',
-		'.mailmap'
+		'tests'
 	],
 	plugins : {
 		'a11yhelp' : 1,
@@ -41,6 +54,7 @@ var CKBUILDER_CONFIG = {
 		'blockquote' : 1,
 		'button' : 1,
 		'clipboard' : 1,
+		'colorbutton' : 1,
 		'contextmenu' : 1,
 		'dialog' : 1,
 		'dialogui' : 1,
@@ -49,14 +63,17 @@ var CKBUILDER_CONFIG = {
 		'entities' : 1,
 		'fakeobjects' : 1,
 		'filebrowser' : 1,
+		'find' : 1,
 		'floatingspace' : 1,
 		'floatpanel' : 1,
+		'font' : 1,
 		'format' : 1,
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
 		'image' : 1,
 		'indent' : 1,
 		'indentlist' : 1,
+		'justify' : 1,
 		'link' : 1,
 		'list' : 1,
 		'listblock' : 1,
@@ -64,10 +81,13 @@ var CKBUILDER_CONFIG = {
 		'maximize' : 1,
 		'menu' : 1,
 		'menubutton' : 1,
+		'newpage' : 1,
 		'panel' : 1,
 		'pastefromword' : 1,
 		'pastetext' : 1,
 		'popup' : 1,
+		'preview' : 1,
+		'print' : 1,
 		'removeformat' : 1,
 		'resize' : 1,
 		'richcombo' : 1,
